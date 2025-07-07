0dbe96dfebb5eb82693427ec25e131cdbd0ac479
Aavej <dev.aavej@gmail.com>
2025-07-07 20:56:52 +0530

    feat: decalare and init the needed  array and variable

---
+++ prob2.java
@@ -0,0 +1,12 @@
+public class prob2 {
+
+// Find the Sum of All Elements
+    public static void main(String[] args) {
+        int nums[]={3, 7, 1, 9};
+        int sum=0;
+        for (int i = 0; i < nums.length; i++) {
+            sum=nums[i]+sum;
+        }
+        System.out.println("Sum of all values is "+sum);
+    }
+}
